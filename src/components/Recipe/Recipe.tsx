9f38deb6fd3e1c1aa4d65cbc47d6ca98be2c1e41
Joa Riski <riski.joa@gmail.com>
2023-11-12 00:12:08 +0200

    sdhouif

--- src/components/Recipe/Recipe.tsx
+++ src/components/Recipe/Recipe.tsx
@@ -14,9 +14,11 @@ export function Recipe(props: RecipeProps) {
   const { data } = props;
   const variant = "solid";
 
+  const bgColor = data.isReady ? "green" : "red";
+
   return (
-    <div className={classnames(styles.root, getVariant(variant))}>
-      <span>{data.label} - Ready: {props.data.isReady.toString()}</span>
+    <div className={classnames(styles.root, getVariant(variant))} style={{ backgroundColor: bgColor }}>
+      <span>{data.label} - Ready: {data.isReady.toString()}</span>
       {props.sourcePosition ? <Handle type="source" position={props.sourcePosition} /> : null}
       {props.targetPosition ? <Handle type="target" position={props.targetPosition} /> : null}
     </div>
